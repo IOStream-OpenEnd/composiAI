88439efb7706a8c70a3599af845347f8090d49cb
Shubham <chhimpa.shubh04@gmail.com>
2019-01-03 20:39:45 +0530

    note_to_octave_list() completed

--- composiai/Octave.py
+++ composiai/Octave.py
@@ -2,8 +2,31 @@ class Octave:
 
     def note_to_octave_list(self, formatted_notes, main_octave):
         note_octave_list = []
+
+        for note in formatted_notes:
+            if len(note) == 2 and not str(note).__contains__('#'):
+                if str(note[0]) == '-':
+                    note = note[1] + str(main_octave - 1)
+                   # print(note)
+                if str(note[0]) == '+':
+                    note = note[1] + str(main_octave + 1)
+                   # print(note)
+            elif len(note) == 2 and str(note).__contains__('#'):
+                note = note + str(main_octave)
+               # print(note)
+            elif len(note) == 1:
+                if note != " ":
+                    note = note + str(main_octave)
+                   # print(note)
+                else:
+                    pass
+            else:
+                print(note, "not => found")
+            note_octave_list.append(note)
+           # print(note_octave_list)
         return note_octave_list
 
-    def get_nth_octave_list(self , octave_number):
+    def get_nth_octave_list(self, octave_number):
         nth_octave_list = []
-        return  nth_octave_list
+
+        return nth_octave_list
