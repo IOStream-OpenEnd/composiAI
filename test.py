ab897de2f4d790a4b2c1a6efd51f0a0b114141c7
Shubham <chhimpa.shubh04@gmail.com>
2019-01-04 19:12:57 +0530

    testing key class

--- test.py
+++ test.py
@@ -1,23 +1,30 @@
 import composiai.Note as n
 import composiai.Octave as o
 import composiai.Frequency as f
+import composiai.Key as k
 import winsound
+
 note_obj = n.Note()
 octave_obj = o.Octave()
+key_obj = k.Key()
 frequency_obj = f.Frequency()
 
 harryPotterNotes = "-B,E,G,F#,E,B,A,F#, , ,E,G,F#,Eb,E,-B"
 got = "G,C,D#,F,G,C,D#,D"
 happy = "D#,C,D,C,F,E,C,C,D,C,G,F,C,C,C,A,F,F,E,D,Bb,Bb,A,F,G,F"
-
+halfgirl = "Bb,+C,+Db,+Eb,+C,Bb,Ab,Ab,Bb,+C,+C"
 
 formatted_notes = note_obj.format_note(happy)
 note_octave_list = octave_obj.note_to_octave_list(formatted_notes, 4)
 note_frequency_dict = frequency_obj.get_note_frequency_dict(note_octave_list, octave_obj)
 
 print(note_frequency_dict)
+print(note_octave_list)
 
 # for windows
-for note in note_octave_list:
-    winsound.Beep(int(note_frequency_dict[note]), 800)  # produces beep sound of evey note for 1 second
+#for note in note_octave_list:
 
+    #key_obj.play(note_frequency_dict[note])
+    #winsound.Beep(int(note_frequency_dict[note]), 800)  # produces beep sound of evey note for 1 second
+
+key_obj.play(note_octave_list,note_frequency_dict)
