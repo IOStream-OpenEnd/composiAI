90861e0c01925a88908f2bc350cc77a2a52d39b8
Shubham <chhimpa.shubh04@gmail.com>
2019-01-04 13:00:31 +0530

    play2 function added

--- composiai/Key.py
+++ composiai/Key.py
@@ -1,11 +1,12 @@
 
 import pyaudio
 import numpy as np
+import wave
 
 
 class Key:
 
-    def play(self, note_octave_list,note_frequency_dict):
+    def play(self, f):
         p = pyaudio.PyAudio()
 
         volume = 0.5  # range [0.0, 1.0]
@@ -13,22 +14,54 @@ class Key:
         duration = 3.0  # in seconds, may be float
         #f = 439.435  # sine frequency, Hz, may be
 
-        for note in note_octave_list:
 
-          # generate samples, note conversion to float32 array
 
-          samples = (np.sin(2 * np.pi * np.arange(fs * duration) * note_frequency_dict[note] / fs)).astype(np.float32)
+        # generate samples, note conversion to float32 array
 
-          # for paFloat32 sample values must be in range [-1.0, 1.0]
-          stream = p.open(format=pyaudio.paFloat32,
+        samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+
+        # for paFloat32 sample values must be in range [-1.0, 1.0]
+        stream = p.open(format=pyaudio.paFloat32,
                         channels=1,
                         rate=fs,
                         output=True)
 
-          # play. May repeat with different volume values (if done interactively)
-          stream.write(volume * samples)
+        # play. May repeat with different volume values (if done interactively)
+        stream.write(volume * samples)
 
         stream.stop_stream()
         stream.close()
 
         p.terminate()
+
+    def  play2(self, note):
+
+        CHUNK = 1024
+
+        wf = wave.open('.\\wav\\' + note + '.wav', 'rb')
+
+        # instantiate PyAudio (1)
+        p = pyaudio.PyAudio()
+
+        # open stream (2)
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+
+        # read data
+        data = wf.readframes(CHUNK)
+
+        # play stream (3)
+        while len(data) > 0:
+            stream.write(data)
+            data = wf.readframes(CHUNK)
+
+        # stop stream (4)
+        stream.stop_stream()
+        stream.close()
+
+        # close PyAudio (5)
+        p.terminate()
+
+
