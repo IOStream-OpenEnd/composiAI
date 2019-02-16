85d1c85f9b15153d68d5d29d5b64a0b78b6dfbab
Shubham <chhimpa.shubh04@gmail.com>
2019-02-16 17:12:42 +0530

    updated sample

--- sample.py
+++ sample.py
@@ -1,17 +1,27 @@
 import composiai.Composer as cp
-
+import os
 
 
 harryPotterNotes = "-B,E,G,F#,E,B,A,F#, , ,E,G,F#,Eb,E,-B"
 got = "G,C,D#,F,G,C,D#,F,G,C,D#,F,D,-G,-A#,C,D,-G,-A#,C,D,-G, , ,G, ,C, ,D#,F,G, ,C, ,D#,F,D,-A#,D#,F,D,-A#, ,F, ,-A#, ,D#,D,F, ,-A#, ,D#,D,C,-G#,-G#,-A#,C,-F,-G#,-A#,C,-F,-G#,-A#,C"
 
 
-myComposer = cp.Composer(harryPotterNotes, 5)
+myComposer = cp.Composer(got, 5)
 #myComposer.play()
 
+output_file_path = os.path.dirname(os.path.realpath(__file__))
 
+output_file_name = "got"
 
-myComposer.export_as_mp3("got","output/","C:\\Users\\Shubham\\Downloads\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe")
+##### To generate mp3 file
+
+myComposer.export_as_wav(output_file_name,output_file_path)
+
+##### To generate mp3 file
+
+#path_to_ffmpeg = "C:\\Users\\Shubham\\Downloads\\ffmpeg\\ffmpeg\\bin\\ffmpeg.exe"
+
+#myComposer.export_as_mp3(output_file_name,output_file_path,path_to_ffmpeg)
 
 
 
